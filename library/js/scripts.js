01e06649b077c105e3875c81bb4cc6af4edb5b3c
Cody W <cody@codywhitby.net>
2016-02-27 13:04:35 -0500

    kinda working i think

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -49,27 +49,68 @@ var $grid = $('.country-grid').isotope({
 });
   
 // store filter for each group
-var filters = [];
+var filters = {};
 
 $('.grid-control').on( 'click', '.checkbox', function() {
   var $this = $(this);
-  var newFilter = $this.attr('data-filter');
-  var found = $.inArray(newFilter, filters);
-  
-  if (found >= 0) {
-    // Element was found, remove it.
-    filters.splice(found, 1);
-  } else {
-    // Element was not found, add it.
-    filters.push(newFilter);
+
+  var group = $this.parents('.grid-control').attr('data-filter-group');
+  // create array for filter group, if not there yet
+  var filterGroup = filters[ group ];
+  if ( !filterGroup ) {
+    filterGroup = filters[ group ] = [];
   }
-  var filtersToString = filters + "";
+
+  // index of
+  var index = $.inArray( $this.attr('data-filter'), filterGroup );
+  if ( index === -1 ) {
+    // add filter to group
+    filters[ group ].push( $this.attr('data-filter') );
+  } else {
+    filters[ group ].splice( index, 1 );
+  }
+
   
-  $grid.isotope({ filter: filtersToString });
+  var comboFilter = getComboFilter( filters );
+
+  $grid.isotope({ filter: comboFilter });
   $this.toggleClass('is-checked');
 });
 
 
+function getComboFilter( filters ) {
+  var i = 0;
+  var comboFilters = [];
+
+  for ( var prop in filters ) {
+    var filterGroup = filters[ prop ];
+    // skip to next filter group if it doesn't have any values
+    if ( !filterGroup.length ) {
+      continue;
+    }
+    if ( i === 0 ) {
+      // copy to new array
+      comboFilters = filterGroup.slice(0);
+    } else {
+      var filterSelectors = [];
+      // copy to fresh array
+      var groupCombo = comboFilters.slice(0); // [ A, B ]
+      // merge filter Groups
+      for (var k=0, len3 = filterGroup.length; k < len3; k++) {
+        for (var j=0, len2 = groupCombo.length; j < len2; j++) {
+          filterSelectors.push( groupCombo[j] + filterGroup[k] ); // [ 1, 2 ]
+        }
+
+      }
+      // apply filter selectors to combo filters for next group
+      comboFilters = filterSelectors;
+    }
+    i++;
+  }
+
+  var comboFilter = comboFilters.join(', ');
+  return comboFilter;
+}
 
 
 }); /* end of as page load scripts */
